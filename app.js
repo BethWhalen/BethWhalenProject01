8042499da117b2c972e765737beb36c2b936411b
Beth <beth.whalen@gmail.com>
2021-07-07 22:34:26 -0300

    made a hamburger menu but still needs edits to toggle

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
+// PSEUDO CODE: HAMBURGER MENU
+// listen for click on the hamburger menu icon
+// when clicked, apply the class of .open to the nav, making the navlinks visible
+// add an X icon for closing the menu (update its class)
+// make the X icon have an event listener so when clicked, it closes the menu and the hamburger returns
+// 
+
+// 
+//declare variables
+const burgerMenu = document.querySelector('.hamburgerMenu');
+const displayNavLinks = document.querySelector('.navLinks');
+const makeTopNavBigger = document.querySelector('.topNap');
+const closeMenuButton = document.querySelector('.closeMenu');
+
+burgerMenu.addEventListener('click', function(){
+    burgerMenu.classList.add('hamburgerMenuOpen')
+    displayNavLinks.classList.add('open');
+    closeMenuButton.classList.add('closeMenuVisible');
+})
+
+function toggleMenu() {
+    if 
+}
+
+
+
+
+
+
 // PSEUDO CODE 
 // To have a comment get posted by a user under the existing blog post comments:
 
-// ---GET SOME SHIT---
+// ---GET---
 // - when the user clicks submit - use an event listener;
 //  --- need to add the prevent default refresh behaviour bit
 // - get the values entered by the user in the form
 
-// ---DO SOME SHIT---
+// ---DO---
 // - take the values and prep them for being added to the page???
 // - somehow get today's date and add it to the h4 
 // - add the users name also to the h4
 // - add the users comment to the article <p>
 // - WHAT ABOUT A USER IMAGE? - can we give them a few images to choose from to add to their comment??
 
-// ---PASS SOME SHIT---
+// ---PASS---
 // - post all the things to the page (date, name, comment, image?)
 
 // console.log('this js is working');
@@ -25,23 +54,23 @@
 
 // const formEl = document.getElementsByClassName('commentForm')[0];
 
-const formEl = document.querySelector('form');
+// const formEl = document.querySelector('form');
 
-formEl.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log('submit form clicked');
+// formEl.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     console.log('submit form clicked');
 
-// 2. get the values the user inputs
-// not sure if this is correct??
-const userName = document.querySelector('[id="name"]');
-//neeed to figure out how to add today's date...
-const commentDate = new Date('need to fix');
+// // 2. get the values the user inputs
+// // not sure if this is correct??
+// const userName = document.querySelector('[id="name"]');
+// //neeed to figure out how to add today's date...
+// const commentDate = new Date('need to fix');
 
-console.log(commentDate);
-
-
+// console.log(commentDate);
 
 
 
-})
+
+
+// })
 
